a8ec69c3e8e9f1eb4f12c88a5c77089e44c66751
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-10-01 13:25:10 +0530

    Create Sum of numbers in a given range.cpp

---
+++ Sum of numbers in a given range.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int num1;
+   int num2;
+   int sum=0;
+   cin>>num1>>num2;
+   for(int i =num1;i<=num2;i++){
+       sum=sum+i;
+   }
+   cout<<sum;
+   
+}
