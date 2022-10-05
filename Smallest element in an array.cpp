caa7cd18be0b9ca823b8f88f8032f58a5068fc0c
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-10-05 20:36:18 +0530

    Create Smallest element in an array.cpp

---
+++ Smallest element in an array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int a[15];
+    
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    /*for(i=0;i<n;i++){
+        cout<<a[i]<< " ";
+    }*/
+    int smallest=a[0];
+    for(int i=0;i<n;i++){
+        if(a[i]<a[0])
+        a[i]=smallest;
+    }
+    cout<<"smallest element is "<<smallest;
+}
