2efbde146320f1fd397939323199c123152a8fd7
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-10-25 11:53:38 +0530

    Create Program to check if a character is alphabet or not.cpp

---
+++ Program to check if a character is alphabet or not.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+char c;
+cin>>c;
+
+if((c>=65&&c<=90)||(c>=97&&c<=122)){
+
+cout<<"It is a alphabet";
+
+}
+
+else{
+cout<<"It is a not a alphabet";
+}
+
+return 0;
+
+
+}
